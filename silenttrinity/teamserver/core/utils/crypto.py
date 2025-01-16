0aa67a25080cf35075b08b01a20ab0725c1f6f60
Aman Khadka <amulifts@gmail.com>
2025-01-17 02:02:02 +0545

    Encrypted Channel

--- silenttrinity/teamserver/core/utils/crypto.py
+++ silenttrinity/teamserver/core/utils/crypto.py
@@ -1,79 +1,113 @@
 # silenttrinity/silenttrinity/teamserver/core/utils/crypto.py
 
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization
-import base64
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes, hmac
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.exceptions import InvalidKey
 import os
+import base64
+from cryptography.hazmat.primitives import serialization
 
 class CryptoManager:
-    """Handles encryption/decryption for TeamServer communications"""
+    """Implements SILENTTRINITY's cryptographic protocol"""
     
     def __init__(self):
-        self._key = None
-        self._fernet = None
+        self._curve = ec.SECP521R1()
         self._private_key = None
         self._public_key = None
+        self._shared_key = None
+        self._session_key = None
+        self._hmac_key = None
         self.initialize()
     
     def initialize(self):
-        """Initialize encryption keys"""
-        # Generate RSA key pair for initial key exchange
-        self._private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
+        """Initialize ECDHE keys"""
+        self._private_key = ec.generate_private_key(self._curve)
         self._public_key = self._private_key.public_key()
-        
-        # Generate session key for Fernet (symmetric) encryption
-        self._key = Fernet.generate_key()
-        self._fernet = Fernet(self._key)
     
     def get_public_key(self):
-        """Get public key in PEM format"""
+        """Export public key for key exchange"""
         return self._public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
     
-    def encrypt_session_key(self, public_key_pem):
-        """Encrypt session key with client's public key"""
-        public_key = serialization.load_pem_public_key(public_key_pem)
-        encrypted_key = public_key.encrypt(
-            self._key,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
+    def perform_key_exchange(self, peer_public_key_bytes):
+        """Perform ECDHE key exchange"""
+        peer_public_key = serialization.load_pem_public_key(peer_public_key_bytes)
+        self._shared_key = self._private_key.exchange(ec.ECDH(), peer_public_key)
+        
+        # Derive session keys using HKDF
+        kdf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=64,  # 32 bytes for AES + 32 bytes for HMAC
+            salt=None,
+            info=b"silenttrinity-v1"
         )
-        return base64.b64encode(encrypted_key)
-    
-    def decrypt_session_key(self, encrypted_key):
-        """Decrypt session key using private key"""
-        encrypted_key = base64.b64decode(encrypted_key)
-        decrypted_key = self._private_key.decrypt(
-            encrypted_key,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-        )
-        return decrypted_key
+        key_material = kdf.derive(self._shared_key)
+        
+        self._session_key = key_material[:32]
+        self._hmac_key = key_material[32:]
     
     def encrypt(self, data):
-        """Encrypt data using Fernet symmetric encryption"""
-        if isinstance(data, str):
+        """Encrypt data using AES-256-CBC with HMAC-SHA256"""
+        if not isinstance(data, bytes):
             data = data.encode()
-        return self._fernet.encrypt(data)
+            
+        # Add PKCS7 padding
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(data) + padder.finalize()
+        
+        # Generate IV
+        iv = os.urandom(16)
+        
+        # Encrypt with AES-256-CBC
+        cipher = Cipher(algorithms.AES(self._session_key), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        
+        # Combine IV and ciphertext
+        encrypted_data = iv + ciphertext
+        
+        # Calculate HMAC
+        h = hmac.HMAC(self._hmac_key, hashes.SHA256())
+        h.update(encrypted_data)
+        mac = h.finalize()
+        
+        # Combine everything and encode
+        return base64.b64encode(encrypted_data + mac)
     
     def decrypt(self, encrypted_data):
-        """Decrypt data using Fernet symmetric encryption"""
-        return self._fernet.decrypt(encrypted_data)
+        """Decrypt data and verify HMAC"""
+        # Decode from base64
+        data = base64.b64decode(encrypted_data)
+        
+        # Split components
+        mac = data[-32:]  # HMAC-SHA256 is 32 bytes
+        encrypted_data = data[:-32]
+        iv = encrypted_data[:16]
+        ciphertext = encrypted_data[16:]
+        
+        # Verify HMAC
+        h = hmac.HMAC(self._hmac_key, hashes.SHA256())
+        h.update(encrypted_data)
+        try:
+            h.verify(mac)
+        except InvalidKey:
+            raise ValueError("Message authentication failed")
+        
+        # Decrypt
+        cipher = Cipher(algorithms.AES(self._session_key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+        
+        # Remove padding
+        unpadder = padding.PKCS7(128).unpadder()
+        return unpadder.update(padded_plaintext) + unpadder.finalize()
     
     def generate_nonce(self):
-        """Generate random nonce for message signing"""
-        return base64.b64encode(os.urandom(16)).decode()
+        """Generate secure random nonce"""
+        return base64.b64encode(os.urandom(32)).decode()
+    
